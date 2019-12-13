d9ee2fb70a11f8dd3294f21a69410620f95851ea
usuarioadmin <usuarioadmin@SAN-G83JF72.usersad.everis.int>
2019-12-13 16:33:04 +0100

    Inicio de Rama master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './content/login/login.component';
+import { InicioComponent } from './content/inicio/inicio.component';
+import { HeaderComponent } from './content/header/header.component';
+import { MaterialModule } from './material/material.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    InicioComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
